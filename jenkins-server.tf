c2f96243bac95f2c65ec086a87a849eb052e8f7a
Alexandru-Vitali.Crutoi.76010082 <Alexandru-Vitali.Crutoi@microfocus.com>
2023-04-17 19:48:48 +0300

    add instance jenkins-server

--- jenkins-server.tf
+++ jenkins-server.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "jenkins-server" {
-  ami           = "ami-0d497a49e7d359666"
-  instance_type = "t2.micro"
+  ami                         = "ami-0d497a49e7d359666"
+  instance_type               = "t2.micro"
   associate_public_ip_address = true
 
   tags = {

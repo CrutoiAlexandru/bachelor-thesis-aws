4019b2bb20698eea77d78017a078d5eef510b495
Alexandru-Vitali.Crutoi.76010082 <Alexandru-Vitali.Crutoi@microfocus.com>
2023-04-17 15:31:14 +0300

    add aws provider

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+  }
+  required_version = ">= 1.1.0"
+
+  cloud {
+    organization = "CrutoiAlexandru"
+
+    workspaces {
+      name = "bachelor-thesis-aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}

d93e4a5fe268b33a8642bf074a3932acedabc5a4
Alexandru-Vitali.Crutoi.76010082 <Alexandru-Vitali.Crutoi@microfocus.com>
2023-04-17 15:47:13 +0300

    add aws budget

--- main.tf
+++ main.tf
@@ -19,3 +19,18 @@ terraform {
 provider "aws" {
   region = "eu-central-1"
 }
+
+resource "aws_budgets_budget" "basic-budget" {
+  name              = "basic-monthly-budget"
+  budget_type       = "COST"
+  limit_amount      = "1"
+  limit_unit        = "USD"
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 100
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "FORECASTED"
+    subscriber_email_addresses = ["crutoi2001@gmail.com"]
+  }
+}

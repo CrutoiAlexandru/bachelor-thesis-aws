ba24c021b638bea9832e75aa51ded406266f9d1d
Alexandru-Vitali.Crutoi.76010082 <Alexandru-Vitali.Crutoi@microfocus.com>
2023-04-17 15:45:08 +0300

    add aws budget

---
+++ budget/budget.tf
@@ -0,0 +1,14 @@
+resource "aws_budgets_budget" "basic-budget" {
+  name              = "basic-monthly-budget"
+  budget_type       = "COST"
+  limit_amount      = "1"
+  limit_unit        = "USD"
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 100
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "FORECASTED"
+    subscriber_email_addresses = ["crutoi2001@gmail.com"]
+  }
+}

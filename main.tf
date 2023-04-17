d3fd7e7d69820fa0272908407fdca757c1a68b50
Alexandru-Vitali.Crutoi.76010082 <Alexandru-Vitali.Crutoi@microfocus.com>
2023-04-17 15:48:04 +0300

    add aws budget

--- main.tf
+++ main.tf
@@ -21,10 +21,10 @@ provider "aws" {
 }
 
 resource "aws_budgets_budget" "basic-budget" {
-  name              = "basic-monthly-budget"
-  budget_type       = "COST"
-  limit_amount      = "1"
-  limit_unit        = "USD"
+  name         = "basic-monthly-budget"
+  budget_type  = "COST"
+  limit_amount = "1"
+  limit_unit   = "USD"
 
   notification {
     comparison_operator        = "GREATER_THAN"

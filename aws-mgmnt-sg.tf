c37441f5e8fcb493195547ec1623e87ead1839fa
Alexandru-Vitali.Crutoi.76010082 <Alexandru-Vitali.Crutoi@microfocus.com>
2023-04-19 15:02:16 +0300

    add instance aws-mgmnt and iam components

---
+++ aws-mgmnt-sg.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "aws-mgmnt-sg" {
+  name_prefix = "aws-mgmnt-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ aws-mgmnt.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "server-mgmnt" {
+  ami           = "ami-0d497a49e7d359666"
+  instance_type = "t2.micro"
+
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.aws-mgmnt-sg.id]
+
+  iam_instance_profile = aws_iam_instance_profile.mgmnt-profile.name
+}

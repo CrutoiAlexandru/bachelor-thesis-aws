814e32982ed9b5bc181d20833f911cb256750079
Alexandru-Vitali.Crutoi.76010082 <Alexandru-Vitali.Crutoi@microfocus.com>
2023-04-19 17:11:07 +0300

    fix traffic

--- aws-mgmnt-sg.tf
+++ aws-mgmnt-sg.tf
@@ -21,11 +21,4 @@ resource "aws_security_group" "aws-mgmnt-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  egress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
